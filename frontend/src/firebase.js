3e7d7e11ca9de67f588ff415bfece995ffd1b7ff
SundaramPandey2005 <145sundarampandey23@gmail.com>
2025-06-28 15:47:39 +0530

    Moved secrets to .env and added to .gitignore

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,15 +5,14 @@ import "firebase/compat/auth"
 import "firebase/compat/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhUkZayOP9fia35z-H-MsOytt3Kl6YMU4",
-  authDomain: "challenge-eeae8.firebaseapp.com",
-  projectId: "challenge-eeae8",
-  storageBucket: "challenge-eeae8.firebasestorage.app",
-  messagingSenderId: "97850040394",
-  appId: "1:97850040394:web:604c56673068d0e8da06b3",
-  measurementId: "G-1K7CN22J4C"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
